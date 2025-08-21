8c3d94fd7b0ee9232626444e059250b3e5128cb2
James Grieve <jamesonrgrieve@gmail.com>
2025-08-21 09:11:01 -0600

    Update setup;

--- podman/setup.sh
+++ podman/setup.sh
@@ -9,12 +9,16 @@ GLPI_DIR="${SCRIPT_DIR}"
 
 printf "=== Setting up GLPI with Podman ===\n"
 
-# Create volume directories in podman user's home
-printf "Creating volume directories...\n"
-mkdir -p "${HOME}/volumes/glpi/config"
-mkdir -p "${HOME}/volumes/glpi/files" 
-mkdir -p "${HOME}/volumes/glpi/plugins"
-mkdir -p "${HOME}/volumes/glpi/mariadb"
+# Volume directories in /srv/glpi (should be created by services.sh with proper permissions)
+GLPI_VOLUMES="/srv/glpi"
+printf "Using volume directory: %s\n" "$GLPI_VOLUMES"
+
+# Verify volume directories exist and are accessible
+if [ ! -d "$GLPI_VOLUMES" ]; then
+    printf "Error: Volume directory %s does not exist or is not accessible\n" "$GLPI_VOLUMES" >&2
+    printf "This should be created by services.sh with proper permissions\n" >&2
+    exit 1
+fi
 
 # Quadlets will manage the containers - no manual podman commands needed
 printf "Quadlets will handle container creation via systemd...\n"
@@ -29,14 +33,14 @@ cp "${GLPI_DIR}/glpi-pod.pod" "${HOME}/.config/containers/systemd/glpi-pod.pod"
 # Copy and configure MariaDB container quadlet from local template
 cp "${GLPI_DIR}/glpi-mariadb.container" "${HOME}/.config/containers/systemd/glpi-mariadb.container"
 sed -i "s|ENV_FILE_PLACEHOLDER|${GLPI_DIR}/glpi.env|g" "${HOME}/.config/containers/systemd/glpi-mariadb.container"
-sed -i "s|MARIADB_VOLUME_PLACEHOLDER|${HOME}/volumes/glpi/mariadb:/var/lib/mysql|g" "${HOME}/.config/containers/systemd/glpi-mariadb.container"
+sed -i "s|MARIADB_VOLUME_PLACEHOLDER|${GLPI_VOLUMES}/mariadb:/var/lib/mysql|g" "${HOME}/.config/containers/systemd/glpi-mariadb.container"
 
 # Copy and configure GLPI app container quadlet from local template
 cp "${GLPI_DIR}/glpi-app.container" "${HOME}/.config/containers/systemd/glpi-app.container"
 sed -i "s|ENV_FILE_PLACEHOLDER|${GLPI_DIR}/glpi.env|g" "${HOME}/.config/containers/systemd/glpi-app.container"
-sed -i "s|CONFIG_VOLUME_PLACEHOLDER|${HOME}/volumes/glpi/config:/var/www/html/config|g" "${HOME}/.config/containers/systemd/glpi-app.container"
-sed -i "s|FILES_VOLUME_PLACEHOLDER|${HOME}/volumes/glpi/files:/var/www/html/files|g" "${HOME}/.config/containers/systemd/glpi-app.container"
-sed -i "s|PLUGINS_VOLUME_PLACEHOLDER|${HOME}/volumes/glpi/plugins:/var/www/html/plugins|g" "${HOME}/.config/containers/systemd/glpi-app.container"
+sed -i "s|CONFIG_VOLUME_PLACEHOLDER|${GLPI_VOLUMES}/config:/var/www/html/config|g" "${HOME}/.config/containers/systemd/glpi-app.container"
+sed -i "s|FILES_VOLUME_PLACEHOLDER|${GLPI_VOLUMES}/files:/var/www/html/files|g" "${HOME}/.config/containers/systemd/glpi-app.container"
+sed -i "s|PLUGINS_VOLUME_PLACEHOLDER|${GLPI_VOLUMES}/plugins:/var/www/html/plugins|g" "${HOME}/.config/containers/systemd/glpi-app.container"
 
 # Reload systemd to recognize new quadlets
 systemctl --user daemon-reload
